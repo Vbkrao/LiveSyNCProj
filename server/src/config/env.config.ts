bc040a5c185f3c1abcf5522fd828e7fa441cd6e3
Bala <82591411+Vbkrao@users.noreply.github.com>
2024-03-05 23:53:45 +0530

    Updated server

---
+++ server/src/config/env.config.ts
@@ -0,0 +1,13 @@
+const env = {
+    NODE_ENV: process.env.NODE_ENV,
+    HOST: process.env.HOST,
+    PORT: process.env.PORT,
+    DATABASE_URL: process.env.DATABASE_URL,
+    USER: process.env.USER,
+    PASSWORD: process.env.PASSWORD,
+    DB_HOST: process.env.DB_PORT,
+    DB_PORT: process.env.DB_PORT,
+    DATABASE: process.env.DATABASE,
+  };
+  
+  export default env;
---
+++ server/src/index.ts
@@ -0,0 +1,25 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import env from "./config/env.config";
+import db from "./db/models";
+import router from "./routes";
+import cors from "cors";
+import errorHandler from "./middleware/error-handler";
+
+dotenv.config();
+
+const app: Express = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(router);
+app.use(errorHandler);
+const port = 8080;
+
+db.sequelize.sync();
+
+
+export default app;
